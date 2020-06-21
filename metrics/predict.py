3deb936c7c571595ffbe95f59a172e1b2076565e
Achleshwar <achleshwarluthra6@gmail.com>
2020-06-21 20:12:30 +0530

    metrics
    similar to sklearn.metrics

---
+++ metrics/predict.py
@@ -0,0 +1,44 @@
+import numpy as np
+import sys
+sys.path.append('../forward_propagation/')
+from forward_propagation import linear_activation_forward
+
+def predict(X, y, parameters):
+    """
+    This function is used to predict the results of a  L-layer neural network.
+    
+    Arguments:
+    X -- data set of examples you would like to label
+    parameters -- parameters of the trained model
+    
+    Returns:
+    p -- predictions for the given dataset X
+    """
+    
+    m = X.shape[1]
+    n = len(parameters) // 2 # number of layers in the neural network
+    p = np.zeros((1,m))
+    
+    # Get W1, b1, W2 and b2 from the dictionary parameters.
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    
+    # Forward propagation
+    A1, cache1 = linear_activation_forward(X, W1, b1, 'relu')
+    A2, cache2 = linear_activation_forward(A1, W2, b2, 'sigmoid')
+    
+    # convert probas to 0/1 predictions
+    for i in range(0, A2.shape[1]):
+        if A2[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+    
+    #print results
+    #print ("predictions: " + str(p))
+    #print ("true labels: " + str(y))
+    print("Accuracy: "  + str(np.sum((p == y)/m)))
+        
+    return p
